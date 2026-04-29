7e7e1d27d16f19c4312fc3b97d5686bd18baea0e
Jack Wu <s2288156@163.com>
2026-04-29 17:41:58 +0800

    chore: remove unused spring-cloud-starter-bootstrap dependency and @EnableDiscoveryClient annotations

--- user-service/src/main/java/org/all/user/UserApplication.java
+++ user-service/src/main/java/org/all/user/UserApplication.java
@@ -2,10 +2,8 @@ package org.all.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
